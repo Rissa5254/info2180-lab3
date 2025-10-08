6f7d51f2b86b8bbe50ccf043fce657afa21ae987
Marissa O'Meally <marissaomeally21@gmail.com>
2025-10-08 16:35:53 -0500

    Add event handling code with X or O to the 3x3 grid Tic-Tac-Toe game board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,42 @@
+//===========Layout Board========
 // Wait for the page to finish load
 window.addEventListener("DOMContentLoaded", function(){
     // Select the board grid
     const board = document.getElementById("board");
 
     // Div inside the game board (3x3 grid, 9 squares)
-    const squares = document.getElementsByTagName("div");
+    const squares = board.getElementsByTagName("div");
 
     // Looping and Adding the square class
     for(let i = 0; i < squares.length; i++){
-        sqaures[i].classList.add("squares");
+        const square  = squares[i];
+        square.classList.add("square");
+
+    //==============Add X or O to Square==========
+
+    // Track game state 
+    let gamestate = new Array(9).fill(null);
+
+    let currentPlayer = 'X';
+    
+    // Clicks square to alternate putting X or O
+    square.addEventListener("click", function(){
+        if(square.textContent === ''){                  // If square is empty
+            square.textContent = currentPlayer;         // Set text it eirther X or O
+            square.classList.add("currentPlayer")       // Add class for styling
+
+            gamestate[i] = currentPlayer;     // Update the game state
+            
+            // Switch Player
+            if (currentPlayer === "X"){       // Alternate between X and O
+                currentPlayer = "O";
+            } else{
+                currentPlayer === "X";
+            }
+        }
+    });
     }
 });
+
+
+
